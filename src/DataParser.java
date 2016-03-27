e4282ca6ea53969ae256e6e0d4290484047ace03
ML <emele18@gmail.com>
2016-03-27 02:46:12 -0400

    DataParser modularized

--- src/DataParser.java
+++ src/DataParser.java
@@ -7,71 +7,45 @@ import java.util.regex.Matcher;
 
 public class DataParser {
 
-    public static List<HashMap<String, Instance> > getFolds() throws IOException {
-        ArrayList<HashMap<String,Instance> > folds = new ArrayList<>(4);
-        Pattern pattern = Pattern.compile("([A-Z]+)\\d+");
+    public static String parseTextFile(String textFilename) throws IOException {
+        BufferedReader textFile = new BufferedReader(new FileReader(textFilename));
+        String textDataRow = textFile.readLine();
+        String fullText = "";
 
-        for (int i = 1; i <= Config.NFOLDS; i++) {
-            HashMap<String, Instance> foldInstances = new HashMap<>();
+        while(textDataRow != null) {
+            fullText += textDataRow;
+            textDataRow = textFile.readLine();
+        }
+        textFile.close();
+        return fullText;
+    }
 
-            for (String topic: Config.TOPICS) {
-                int countOpinions = 1;
-                String foldFilename = Config.FOLDS_ROOT + topic + "_folds/Fold-" + i;
-                System.out.println(foldFilename);
-                BufferedReader foldFile = new BufferedReader(new FileReader(foldFilename));
-                String foldDataRow = foldFile.readLine();
+    public static Opinion parseMetaFile(String metaFilename) throws IOException {
+        Opinion opinion = new Opinion();
+        BufferedReader metaFile = new BufferedReader(new FileReader(metaFilename));
+        String metaDataRow = metaFile.readLine();
 
-                while (foldDataRow != null) {
-                    //System.out.println(countOpinions + ": " + foldDataRow);
-                    String textFilename = Config.DATA_ROOT + topic + "/" + foldDataRow + ".data";
-                    BufferedReader textFile = new BufferedReader(new FileReader(textFilename));
-                    String textDataRow = textFile.readLine();
-                    String fullText = "";
-
-                    while(textDataRow != null) {
-                        fullText += textDataRow;
-                        textDataRow = textFile.readLine();
-                    }
-                    textFile.close();
-
-                    String metaFilename = Config.DATA_ROOT + topic + "/" + foldDataRow + ".meta";
-                    BufferedReader metaFile = new BufferedReader(new FileReader(metaFilename));
-                    String metaDataRow = metaFile.readLine();
-                    Opinion opinion = new Opinion();
-                    opinion.text = fullText;
-
-                    while (metaDataRow != null) {
-                        String[] data = metaDataRow.split("=");
-                        opinion.topic = topic;
-                        Matcher m = pattern.matcher(foldDataRow);
-                        m.find();
-                        opinion.debate = m.group(1);
+        while (metaDataRow != null) {
+            String[] data = metaDataRow.split("=");
                         switch(data[0]) {
-                            case "ID":
-                                opinion.id = Integer.parseInt(data[1]);
-                            case "PID":
-                                opinion.pid = Integer.parseInt(data[1]);
-                            case "Stance":
-                                opinion.stance = Integer.parseInt(data[1]);
-                            case "rebuttal":
-                                opinion.rebuttal = data[1];
-                        }
-                        metaDataRow = metaFile.readLine();
-                    }
-
-                    metaFile.close();
-                    foldInstances.put(opinion.debate + opinion.id, opinion);
-                    countOpinions += 1;
-                    foldDataRow = foldFile.readLine();
-
-                }
-                foldFile.close();
+                case "ID":
+                    opinion.id = Integer.parseInt(data[1]);
+                case "PID":
+                    opinion.pid = Integer.parseInt(data[1]);
+                case "Stance":
+                    opinion.stance = Integer.parseInt(data[1]);
+                case "rebuttal":
+                    opinion.rebuttal = data[1];
             }
-            folds.add(foldInstances);
+            metaDataRow = metaFile.readLine();
         }
 
-        for (String topic: Config.TOPICS) {
+        metaFile.close();
+        return opinion;
+    }
 
+    public static void parseAuthorFile(List<HashMap<String, Instance> > folds) throws IOException {
+        for (String topic: Config.TOPICS) {
             System.out.println("Authors for " + topic);
 
             File dir = new File(Config.AUTHOR_ROOT + topic);
@@ -92,9 +66,51 @@ public class DataParser {
                 }
                 authorFile.close();
             }
+
+        }
+    }
+
+    public static List<HashMap<String, Instance> > getFolds() throws IOException {
+        ArrayList<HashMap<String,Instance> > folds = new ArrayList<>(4);
+        Pattern pattern = Pattern.compile("([A-Z]+)\\d+");
+
+        for (int i = 1; i <= Config.NFOLDS; i++) {
+            HashMap<String, Instance> foldInstances = new HashMap<>();
+
+            for (String topic: Config.TOPICS) {
+                int countOpinions = 1;
+                String foldFilename = Config.FOLDS_ROOT + topic + "_folds/Fold-" + i;
+                System.out.println(foldFilename);
+                BufferedReader foldFile = new BufferedReader(new FileReader(foldFilename));
+                String foldDataRow = foldFile.readLine();
+
+                while (foldDataRow != null) {
+                    // get opinion text in .data file
+                    String textFilename = Config.DATA_ROOT + topic + "/" + foldDataRow + ".data";
+                    String fullText = parseTextFile(textFilename);
+
+                    // opinion meta data in .meta file
+                    String metaFilename = Config.DATA_ROOT + topic + "/" + foldDataRow + ".meta";
+                    Opinion opinion = parseMetaFile(metaFilename);
+
+                    // other opinion data
+                    opinion.text = fullText;
+                    opinion.topic = topic;
+                    Matcher m = pattern.matcher(foldDataRow);
+                    m.find();
+                    opinion.debate = m.group(1);
+
+                    foldInstances.put(opinion.debate + opinion.id, opinion);
+                    countOpinions += 1;
+                    foldDataRow = foldFile.readLine();
+
+                }
+                foldFile.close();
+            }
+            folds.add(foldInstances);
         }
 
-
+        parseAuthorFile(folds);
         return folds;
     }
 
